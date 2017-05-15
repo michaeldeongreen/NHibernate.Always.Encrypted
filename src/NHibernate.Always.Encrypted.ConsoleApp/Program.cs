18dad50202b1f926e73b955fd46c8e1cfdb0f75a
michaeldeongreen <michaeldeongreen@gmail.com>
2017-05-15 09:06:53 -0500

    *Verified that I can save unencrypted

--- src/NHibernate.Always.Encrypted.ConsoleApp/Program.cs
+++ src/NHibernate.Always.Encrypted.ConsoleApp/Program.cs
@@ -11,8 +11,22 @@ namespace NHibernate.Always.Encrypted.ConsoleApp
     {
         static void Main(string[] args)
         {
+
             using (ISession session = NHibernateSession.OpenSession())
             {
+                session.SaveOrUpdate(new Patient()
+                {
+                     BirthDate = DateTime.Now,
+                     FirstName = "Michael",
+                     City = "Dallas",
+                     LastName = "Green",
+                     MiddleName = "D",
+                     SSN = "638-55-8787",
+                     State = "TX",
+                     StreetAddress = "4902 Hibiscus Dr",
+                     ZipCode = "75521"
+                });
+
                 var employee = session.QueryOver<Patient>().Where(p => p.Id == 1).SingleOrDefault();
             }
 
